b9c43561e709fb03cb338607413406e11538d035
aott33 <48938478+aott33@users.noreply.github.com>
2019-03-27 14:12:45 -0700

    Add files via upload

--- Gripper_Control.ino
+++ Gripper_Control.ino
@@ -5,26 +5,31 @@
  *  This program controls a 3D printed gripper for a kuka robot arm.
  *  The servo is able to open and close the gripper and a feedback
  *  loop is made with the use of force sensing resistors.
+ *  
+ *  Analog value for gripper(cup): 750
  */
 
-#include <servo.h>
+#include <Servo.h>
 
 //creates servo object to control a servo
 Servo servo;
 
-//servo signal connect to digital pin 8
-int gripClose= 8;
+//pins 10 and 11 control servo operation
+int gripClose= 7;
+int gripOpen= 11;
+
+//pin states
+int ipClose=0;
+int ipOpen=0;
 
 //force sensor connected to pin 9
-int fsrIn= 9;
+int fsrIn= 8;
 int fsrReading;
-
-int gripOpen= 10;
 
 void setup() 
 {
   Serial.begin(9600);
-  
+
   //attaches servo connected at pin 2 to the servo object
   servo.attach(2);
 
@@ -33,33 +38,44 @@ void setup()
 
   //sets gripOpen(pin 10) to be a digital input
   pinMode(gripOpen, INPUT);
+
+  digitalWrite (gripClose, HIGH);
+  digitalWrite (gripOpen, HIGH);
 }
 
 void loop() 
-{
+{  
   //initializes fsrReading
   fsrReading = analogRead(fsrIn);
+  ipClose = digitalRead(gripClose);
+  ipOpen = digitalRead(gripOpen);
 
   //prints analog values of fsr
   Serial.print ("Analog reading= ");
-  Serial.print (fsrReading);
+  Serial.println (fsrReading);
+
+  //prints states of digital inputs
+  Serial.print("Close Command: ");
+  Serial.println (ipClose);
+  Serial.print("Open Command: ");
+  Serial.println (ipOpen);
+  
   
   //closes gripper if a high is received to gripClose(pin 8)
-  if (gripClose == HIGH)
+  if (ipClose == HIGH && fsrReading < 750)
   {
-    servo.write(1300);
+    servo.writeMicroseconds(1700);
   }
-
-  //stops closing gripper once a certain level of force is achieved
-  else if (fsrReading >= 500)
-  {
-    servo.write(1450);
-  }
-
+  
   //opens gripper once if a high is received to gripOpen(pin 10)
-  else if (gripOpen == HIGH)
+  else if (ipOpen == HIGH)
   {
-    servo.write(1700);
+    servo.writeMicroseconds(1300);
+  }
+  
+  else
+  {
+    servo.writeMicroseconds(1510);
   }
   
   delay (100);  
